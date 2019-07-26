0c0aac487cd3190d9d1d3346f7f825b1999f42e5
Unknown <renatassmendes@hotmail.com>
2019-07-26 03:00:16 -0300

    Listando acessorios e calculando preco total

--- Ionic-aluracar/src/pages/escolha/escolha.ts
+++ Ionic-aluracar/src/pages/escolha/escolha.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Carro } from '../../modelos/carro';
+import { Acessorio } from '../../modelos/acessorio';
 
 /**
  * Generated class for the EscolhaPage page.
@@ -17,15 +18,30 @@ import { Carro } from '../../modelos/carro';
 export class EscolhaPage {
 
   public carro: Carro;
+  public acessorios: Acessorio[];
+  private _precoTotal: number;
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams) {
 
       this.carro = this.navParams.get('carroSelecionado')
+      this._precoTotal = this.carro.preco;
+      this.acessorios = [
+        { nome: 'Freio ABS', preco: 800 },
+        { nome : 'Ar-Condicionado', preco: 1000 },
+        { nome: 'MP3 Player', preco: 500 }
+      ]
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EscolhaPage');
+  atualizaTotal(ativado:boolean, acessorio: Acessorio){
+    ativado ?
+      this._precoTotal += acessorio.preco:
+        this._precoTotal -= acessorio.preco;
   }
+
+  get precoTotal(){
+    return this._precoTotal;
+  }
+
 
 }
