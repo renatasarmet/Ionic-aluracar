6576a9eb2273352d18eb389c2656986286733088
Unknown <renatassmendes@hotmail.com>
2019-07-26 02:43:14 -0300

    Selecionando carro e indo para detalhes

--- Ionic-aluracar/src/pages/home/home.ts
+++ Ionic-aluracar/src/pages/home/home.ts
@@ -4,6 +4,7 @@ import { Carro } from '../../modelos/carro';
 import { HttpErrorResponse } from '@angular/common/http';
 import { CarrosServiceProvider } from '../../providers/carros-service/carros-service';
 import { NavLifecycles } from '../../utils/ionic/nav/nav-lifecycles';
+import { EscolhaPage } from '../escolha/escolha';
 
 @Component({
   selector: 'page-home',
@@ -45,4 +46,12 @@ export class HomePage implements NavLifecycles {
                 }
               )
   }
+
+  selecionaCarro(carro: Carro){
+    console.log(carro);
+    this.navCtrl.push(EscolhaPage.name, {
+      carroSelecionado: carro
+    });
+  }
+
 }
